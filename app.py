bdfd1563ae2a6a08ad573ebda1a8ea533b3bea2a
Rojojun <zoloman316@gmail.com>
2022-05-11 23:37:33 +0900

    [feature] 로그인 전용 링크 재생성 -> 한글화 되어 있는 링크를 간결하게 작성

--- app.py
+++ app.py
@@ -120,5 +120,10 @@ def recommend_post():
 #
 #     return jsonify({'recommends': recommend_list})
 
+#로그인 기본 페이지 링크
+@app.route('/login_page')
+def login_page():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
